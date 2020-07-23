70653e7953e0bc422fb7985e266bd4c954cf2516
himanshuharlalka <himanshuharlalka@gmail.com>
2020-07-23 21:32:06 +0530

    added database, volley for local news

--- app/src/main/java/com/example/news/data/DatabaseHandler.java
+++ app/src/main/java/com/example/news/data/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.example.news.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,6 +10,9 @@ import androidx.annotation.Nullable;
 
 import com.example.news.model.Article;
 import com.example.news.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -42,6 +46,29 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         values.put(Util.KEY_DESCRIPTION,article.getDescription());
         values.put(Util.KEY_ARTICLE_URL,article.getArticle_url());
         db.insert(Util.TABLE_NAME,null,values);
+        db.close();
+    }
+    public List<Article> getAllArticles(){
+        SQLiteDatabase db=this.getReadableDatabase();
+        List<Article> articleList=new ArrayList<>();
+        String selectAll="SELECT * FROM "+Util.TABLE_NAME;
+        Cursor cursor=db.rawQuery(selectAll,null);
+        if(cursor.moveToFirst()){
+            do {
+                Article article=new Article();
+                article.setId(cursor.getInt(0));
+                article.setTitle(cursor.getString(1));
+                article.setImage_url(cursor.getString(2));
+                article.setDescription(cursor.getString(3));
+                article.setArticle_url(cursor.getString(4));
+                articleList.add(article);
+            }while (cursor.moveToNext());
+        }
+        return articleList;
+    }
+    public void deleteArticle(Article article){
+        SQLiteDatabase db=this.getWritableDatabase();
+        db.delete(Util.TABLE_NAME,Util.KEY_ID+"=?",new String[]{String.valueOf(article.getId())});
         db.close();
     }
 
